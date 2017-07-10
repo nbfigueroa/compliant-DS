f4b65b92dd2932530d6701d36087a5d9588d228f
nbfigueroa <nbfigueroa@gmail.com>
2017-07-10 21:51:32 +0200

    Latest updates

--- demo_surface_estimate.m
+++ demo_surface_estimate.m
@@ -17,50 +17,6 @@ titlename = strcat(dataset_name,' Demonstrations (',labels,')');
 if exist('h7','var') && isvalid(h7), delete(h7);end
 h7 = plotLabeled3DTrajectories(Data, True_states, titlename, label_range);
 axis tight
-
-%% Compute more features for Data Analysis
-demo_id = 1;
-data    = Data{demo_id}';
-phases  = True_states{demo_id}(1:end-1)';
-
-% Convert positions to velocities
-position    = data(1:3,1:end-2);
-velocity   = [diff(position(1,:));diff(position(2,:));diff(position(3,:))];
-velocity   = sgolayfilt(velocity', 3, 151)';
-
-% Convert positions to velocities
-forces     = data(8:10,1:end-1);
-
-% Sample
-sample = 20;
-
-% Plot original data
-plot_options              = [];
-plot_options.is_eig       = false;
-plot_options.labels       = phases(1:sample:end);
-plot_options.points_size  = 10;
-plot_options.class_names  = {'Reach', 'Roll','Back'};
-plot_options.title        = 'Position Domain  Rolling Demonstrations';
-
-if exist('h1','var') && isvalid(h1), delete(h1);end
-h1 = ml_plot_data(position(:,1:sample:end)',plot_options);
-hold on;
-
-% Plot velocities
-X = position(1,1:sample:end)'; Y = position(2,1:sample:end)'; Z = position(3,1:sample:end)';
-U = velocity(1,1:sample:end)'; V = velocity(2,1:sample:end)'; W = velocity(3,1:sample:end)';
-quiver3(X,Y,Z,U,zeros(size(U)),zeros(size(U)),'color',[0,0,1],'linewidth',.25); hold on;
-quiver3(X,Y,Z,zeros(size(U)),V,zeros(size(U)),'color',[0,0,1],'linewidth',.25); hold on;
-quiver3(X,Y,Z,zeros(size(U)),zeros(size(U)),W,'color',[0,0,1],'linewidth',.25); hold on;
-quiver3(X,Y,Z,U,V,W,'color',[0,1,1],'linewidth',.25); hold on;
-
-% Plot forces
-Fx = forces(1,1:sample:end)'; Fy = forces(2,1:sample:end)'; Fz = forces(3,1:sample:end)';
-quiver3(X,Y,Z,Fx,zeros(size(Fx)),zeros(size(Fx)),'color',[1,0,0],'linewidth',.25); hold on;
-quiver3(X,Y,Z,zeros(size(Fx)),Fy,zeros(size(Fx)),'color',[1,0,0],'linewidth',.25); hold on;
-quiver3(X,Y,Z,zeros(size(Fz)),zeros(size(Fz)),Fz,'color',[1,0,0],'linewidth',.25);
-quiver3(X,Y,Z,Fx,Fy,Fz,'color',[0,1,1],'linewidth',.25); hold on;
-
 %% 3) Real 'Peeling' (max) 32-D dataset, 5 Unique Emission models, 3 time-series
 % Demonstration of a Bimanual Peeling Task consisting of 
 % 3 (32-d) time-series X = {x_1,..,x_T} with variable length T. 
@@ -90,6 +46,104 @@ use_vel = 0;
 [data, TruePsi, Data, True_states, Data_] = load_peeling_dataset( data_path, dim, display, normalize, weights, use_vel);
 dataset_name = 'Peeling';
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%     Data Analysis for physically-inspired discretization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute more features for Data Analysis
+demo_id = 3;
+data    = Data{demo_id}';
+phases  = True_states{demo_id}(1:end-1)';
 
+% Convert positions to velocities
+position    = data(1:3,1:end-2);
+velocity   = [diff(position(1,:));diff(position(2,:));diff(position(3,:))];
+velocity   = sgolayfilt(velocity', 3, 151)';
 
+% Convert positions to velocities
+forces     = data(8:10,1:end-1);
 
+% Sample
+sample = 10;
+
+% Plot original data
+plot_options              = [];
+plot_options.is_eig       = false;
+plot_options.labels       = phases(1:sample:end);
+plot_options.points_size  = 20;
+plot_options.class_names  = {'Reach', 'Roll','Back'};
+plot_options.plot_labels  = {'x','y','z'};
+plot_options.title        = 'Position Domain  Rolling Demonstrations';
+
+if exist('h1','var') && isvalid(h1), delete(h1);end
+h1 = ml_plot_data(position(:,1:sample:end)',plot_options);
+hold on;
+
+% Plot resultant velocity vector
+X = position(1,1:sample:end)'; Y = position(2,1:sample:end)'; Z = position(3,1:sample:end)';
+U = velocity(1,1:sample:end)'; V = velocity(2,1:sample:end)'; W = velocity(3,1:sample:end)';
+quiver3(X,Y,Z,U,V,W,'color',[0,0,1],'linewidth',.25); hold on;
+
+% Normalize velocity directions
+for i=1:length(U)
+    vel = [U(i) V(i) W(i)]/norm([U(i) V(i) W(i)]);
+    U(i) = vel(1);
+    V(i) = vel(2);
+    W(i) = vel(3);
+end
+
+% Plot normalized velocity directions
+quiver3(X,Y,Z,U,zeros(size(U)),zeros(size(U)),'color',[0,0.5,1],'linewidth',.25); hold on;
+quiver3(X,Y,Z,zeros(size(U)),V,zeros(size(U)),'color',[0,0.5,1],'linewidth',.25); hold on;
+quiver3(X,Y,Z,zeros(size(U)),zeros(size(U)),W,'color',[0,0.5,1],'linewidth',.25); hold on;
+
+% Plot resultant forces
+Fx = forces(1,1:sample:end)'; Fy = forces(2,1:sample:end)'; Fz = forces(3,1:sample:end)';
+% quiver3(X,Y,Z,Fx,Fy,Fz,'color',[1,0,0],'linewidth',.25); hold on;
+
+approx_frict = zeros(size(Fx));
+dotxy = zeros(size(Fx));
+dotxz = zeros(size(Fy));
+dotyz = zeros(size(Fz));
+for i=1:length(U)
+    % Estimate approx. friction coeff
+    if Fz(i) > 0
+        approx_frict(i) = sqrt(Fx(i)^2 + Fy(i)^2)/Fz(i);
+    end
+    
+    % Normalize force directions
+    force = [Fx(i) Fy(i) Fz(i)]/norm([Fx(i) Fy(i) Fz(i)]);
+    Fx(i) = force(1);
+    Fy(i) = force(2);
+    Fz(i) = force(3);
+   
+    % Dot-product of normalized directions
+    dotxy(i) = dot([U(i) V(i)],[Fx(i) Fy(i)]); 
+    dotxz(i) = dot([U(i) W(i)],[Fx(i) Fz(i)]);
+    dotyz(i) = dot([V(i) W(i)],[Fy(i) Fz(i)]);
+    
+end
+
+% Plot normalized force directions
+quiver3(X,Y,Z,Fx,zeros(size(Fx)),zeros(size(Fx)),'color',[1,0.25,0],'linewidth',.25); hold on;
+quiver3(X,Y,Z,zeros(size(Fx)),Fy,zeros(size(Fx)),'color',[1,0.25,0],'linewidth',.25); hold on;
+quiver3(X,Y,Z,zeros(size(Fz)),zeros(size(Fz)),Fz,'color',[1,0.25,0],'linewidth',.25);
+
+% Plot Approx Friction Coefficient
+if exist('h2','var') && isvalid(h2), delete(h2);end
+h2 = figure('Color',[1 1 1]);
+subplot(3,1,1)
+stairs(phases(1:sample:end), 'LineWidth',2); 
+title('Phases','Interpreter', 'LaTex', 'FontSize',20)
+grid on;
+subplot(3,1,2)
+plot(approx_frict, 'LineWidth',2); hold on;
+plot(ones(length(approx_frict)), 'LineWidth',2, 'Color', [0 0 0]); hold on;
+title('Approximate Friction $\tilde{\mu} = (f_x^2+f_y^2)^{(-1/2)}/f_z$','Interpreter', 'LaTex', 'FontSize',20)
+grid on;
+subplot(3,1,3)
+plot(dotxy, 'LineWidth',2); hold on;
+plot(dotxz, 'LineWidth',2); hold on;
+plot(dotyz, 'LineWidth',2); 
+legend({'$f_{xy}/||f_{xy}|| \cdot v_{xy}/||v_{xy}||$','$f_{xz}/||f_{xz}|| \cdot v_{yz}/||v_{yz}||$','$f_{yz}/||f_{yz}|| \cdot v_{yz}/||v_{yz}||$'},'Interpreter', 'LaTex')
+title('Dot products of normalized directions','Interpreter', 'LaTex', 'FontSize',20)
+grid on;
