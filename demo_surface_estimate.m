2f64762992fec52aaa4db644740fb7813c61f885
nbfigueroa <nbfigueroa@gmail.com>
2017-07-10 23:46:29 +0200

    Peeling dataset and toy dataset working

--- demo_surface_estimate.m
+++ demo_surface_estimate.m
@@ -1,5 +1,17 @@
 %% 1) 2D Simulated Dataset from Sina's CODS Toolbox
+data_path = './test-data/';
 
+load(strcat(data_path,'Toy/Toy_Data.mat'))
+openfig(strcat(data_path,'Toy/Toy_simulation.fig'))
+
+%% Concatenate Data
+for i=1:length(X_modulated)
+    Data{i} = [X_modulated{i}(1,1:10:end); zeros(size(X_modulated{i}(1,1:10:end))); X_modulated{i}(2,1:10:end); ...
+        -0.5*F_modulated{i}(1,1:10:end); zeros(size(X_modulated{i}(1,1:10:end)));  F_modulated{i}(1,1:10:end)];
+    True_states{i} = 2*ones(length(Data{i}));
+    free_motion = F_modulated{i}(1,:)==0;
+    True_states{i}(1,free_motion) = 1;    
+end
 
 %% 2) Real 'Dough-Rolling' 12D dataset, 3 Unique Emission models, 12 time-series
 % Demonstration of a Dough Rolling Task consisting of 
@@ -13,38 +25,26 @@ dataset_name = 'Rolling'; demos = [1:2]; labels = 'human'; % 'human'/'icsc-hmm';
 label_range = [1:3];
 
 % Plot Segmentated 3D Trajectories
-titlename = strcat(dataset_name,' Demonstrations (',labels,')');
-if exist('h7','var') && isvalid(h7), delete(h7);end
-h7 = plotLabeled3DTrajectories(Data, True_states, titlename, label_range);
+titlename = strcat(dataset_name,' Demonstrations (Segmented)');
+if exist('h0','var') && isvalid(h0), delete(h0);end
+h0 = plotLabeled3DTrajectories(Data, True_states, titlename, label_range);
 axis tight
-%% 3) Real 'Peeling' (max) 32-D dataset, 5 Unique Emission models, 3 time-series
+%% 3) Real 'Peeling' (max) 13-D dataset, 5 Unique Emission models, 3 time-series
 % Demonstration of a Bimanual Peeling Task consisting of 
 % 3 (32-d) time-series X = {x_1,..,x_T} with variable length T. 
 % Dimensions:
-clc; 
-clear all; close all
-data_path = '../ICSC-HMM/test-data/'; display = 0; 
+clc; clear all; close all;
+data_path = './test-data/'; type = 'aligned'; % 'aligned'/'rotated'
+dataset_name = 'Peeling'; demos = [1]; labels = 'human'; % 'human'/'icsc-hmm'; 
 
-% Type of data processing
-% O: no data manipulation -- 1: zero-mean -- 2: scaled by range * weights
-normalize = 2; 
+[Data, True_states] = load_peeling_demos( data_path, demos, labels);
+label_range = [2:3];
 
-% Select dimensions to use
-dim = 'active'; 
-
-% Define weights for dimensionality scaling
-weights = [3*ones(1,3) 1/2*ones(1,4) 1/15*ones(1,3) 1/2*ones(1,3)]';
-switch dim                
-    case 'active'
-    case 'robots' 
-        weights = [weights 1/3*ones(1,3) 2*ones(1,4) 1/15*ones(1,3) 1/5*ones(1,3)]';        
-end
-
-% Define if using first derivative of pos/orient
-use_vel = 0;
-
-[data, TruePsi, Data, True_states, Data_] = load_peeling_dataset( data_path, dim, display, normalize, weights, use_vel);
-dataset_name = 'Peeling';
+% Plot Segmentated 3D Trajectories
+titlename = strcat(dataset_name,' Demonstrations (Segmented)');
+if exist('h0','var') && isvalid(h0), delete(h0);end
+h0 = plotLabeled3DTrajectories(Data, True_states, titlename, label_range);
+axis tight
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%     Data Analysis for physically-inspired discretization
@@ -52,18 +52,18 @@ dataset_name = 'Peeling';
 % Compute more features for Data Analysis
 demo_id = 3;
 data    = Data{demo_id}';
-phases  = True_states{demo_id}(1:end-1)';
+phases  = True_states{demo_id}(1:end-2)';
 
 % Convert positions to velocities
-position    = data(1:3,1:end-2);
+position    = data(1:3,1:end-1);
 velocity   = [diff(position(1,:));diff(position(2,:));diff(position(3,:))];
 velocity   = sgolayfilt(velocity', 3, 151)';
-
+position    = data(1:3,1:end-2);
 % Convert positions to velocities
-forces     = data(8:10,1:end-1);
+forces     = data(8:10,1:end-2);
 
 % Sample
-sample = 10;
+sample = 1;
 
 % Plot original data
 plot_options              = [];
---
+++ test-data/Peeling/load_peeling_demos.m
@@ -0,0 +1,40 @@
+function [Data_seq True_states_seq] = load_peeling_demos( data_path, demos, labels)
+
+% Load All Time-Series Data and Labels
+load(strcat(data_path,'Peeling/proc-data-labeled.mat'))
+load(strcat(data_path,'Peeling/proc-labels.mat'))
+load(strcat(data_path,'Peeling/proc-data-noObj.mat'))
+Data{4} = Data_noObj{1}; True_states{4} = True_states_noObj{1};
+Data{5} = Data_noObj{2}; True_states{5} = True_states_noObj{2};
+
+
+% Load label sequences
+switch labels
+    case 'human'
+        load(strcat(data_path,'Peeling/Sequence_labels.mat'))
+        
+    case 'icsc-hmm'
+end
+
+% Extract peeling sequences from selected demonstrations
+Data_  = Data; True_states_ = True_states;
+clear Data True_states
+Total_sequences = 0; j = 1;
+
+for i=1:length(demos)
+    Data{i} = Data_{i}(1:13,:)';
+    True_states{i} = True_states_{i}';
+    Total_sequences = Total_sequences + size(Seq{demos(i)},1);
+    X = Data{i}; segs = Seq{demos(i)};    
+    true_states = True_states{i};
+    for k = 1:size(Seq{demos(i)},1)  
+        ids = segs(k,1):segs(k,2);
+        Data_seq{j} = X(ids,:);
+        True_states_seq{j} = true_states(ids)';
+        j = j + 1;
+    end
+end
+
+end
+
+
