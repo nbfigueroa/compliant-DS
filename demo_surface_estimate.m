59db624c4922345c3ab15cccc020be20c299c99c
nbfigueroa <nbfigueroa@gmail.com>
2017-07-11 11:40:48 +0200

    Toy and rolling data working

--- demo_surface_estimate.m
+++ demo_surface_estimate.m
@@ -1,5 +1,5 @@
 %% 1) 2D Simulated Dataset from Sina's CODS Toolbox
-clear all; clc;
+clc; clear all; close all;
 data_path = './test-data/'; dataset_name = 'Toy';
 
 load(strcat(data_path,'Toy/Toy_Data.mat'))
@@ -7,11 +7,12 @@ openfig(strcat(data_path,'Toy/Toy_simulation.fig'))
 
 for i=1:length(X_modulated)
     Data{i} = [X_modulated{i}(1,1:10:end); zeros(size(X_modulated{i}(1,1:10:end))); X_modulated{i}(2,1:10:end); zeros(4,length(X_modulated{i}(1,1:10:end))); ...        
-        -0.85*F_modulated{i}(1,1:10:end); 0.05*F_modulated{i}(1,1:10:end);  F_modulated{i}(1,1:10:end)]';
+        -0.85*F_modulated{i}(2,1:10:end); 0.25*F_modulated{i}(2,1:10:end);  F_modulated{i}(2,1:10:end)]';    
     True_states{i} = 2*ones(1,length(Data{i}))';
     for k=1:length(Data{i})
         if Data{i}(k,10) == 0 
             True_states{i}(k,1) = 1;    
+            Data{i}(k,8:10) = -0.5*Data{i}(k,1:3);
         end
     end    
 end
@@ -60,7 +61,7 @@ axis tight
 %%     Data Analysis for physically-inspired discretization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute more features for Data Analysis
-demo_id = 10;
+demo_id = 3;
 data    = Data{demo_id}';
 phases  = True_states{demo_id}(1:end-2)';
 
@@ -69,11 +70,12 @@ position    = data(1:3,1:end-1);
 velocity   = [diff(position(1,:));diff(position(2,:));diff(position(3,:))];
 velocity   = sgolayfilt(velocity', 3, 151)';
 position    = data(1:3,1:end-2);
+
 % Convert positions to velocities
 forces     = data(8:10,1:end-2);
 
 % Sample
-sample = 1;
+sample = 20;
 
 % Plot original data
 plot_options              = [];
@@ -81,12 +83,12 @@ plot_options.is_eig       = false;
 plot_options.labels       = phases(1:sample:end);
 plot_options.points_size  = 20;
 plot_options.plot_labels  = {'x','y','z'};
-plot_options.title        = 'Position DomainDemonstrations';
+plot_options.title        = 'Force-Motion Directions';
 
 if exist('h1','var') && isvalid(h1), delete(h1);end
 h1 = ml_plot_data(position(:,1:sample:end)',plot_options);
 hold on;
-% ylim([-0.5 1])
+
 % Plot resultant velocity vector
 X = position(1,1:sample:end)'; Y = position(2,1:sample:end)'; Z = position(3,1:sample:end)';
 U = velocity(1,1:sample:end)'; V = velocity(2,1:sample:end)'; W = velocity(3,1:sample:end)';
@@ -101,13 +103,13 @@ for i=1:length(U)
 end
 
 % % Plot normalized velocity directions
-quiver3(X,Y,Z,U,zeros(size(U)),zeros(size(U)),'color',[0,0.5,1],'linewidth',.5); hold on;
+quiver3(X,Y,Z,U,zeros(size(U)),zeros(size(U)),'color',[0,1,1],'linewidth',.5); hold on;
 quiver3(X,Y,Z,zeros(size(U)),V,zeros(size(U)),'color',[0,0.5,1],'linewidth',.5); hold on;
-quiver3(X,Y,Z,zeros(size(U)),zeros(size(U)),W,'color',[0,0.5,1],'linewidth',.5); hold on;
+quiver3(X,Y,Z,zeros(size(U)),zeros(size(U)),W,'color',[0,1,1],'linewidth',.5); hold on;
 
 % Plot resultant forces
 Fx = forces(1,1:sample:end)'; Fy = forces(2,1:sample:end)'; Fz = forces(3,1:sample:end)';
-quiver3(X,Y,Z,Fx,Fy,Fz,'color',[1,0,0],'linewidth',.5); hold on;
+quiver3(X,Y,Z,Fx,Fy,Fz,'color',[1,0.75,0],'linewidth',.5); hold on;
 
 approx_frict_x = zeros(size(Fx));
 approx_frict_y = zeros(size(Fx));
@@ -140,32 +142,39 @@ for i=1:length(U)
     
 end
 
-% Plot normalized force directions
+% % Plot normalized force directions
 quiver3(X,Y,Z,Fx,zeros(size(Fx)),zeros(size(Fx)),'color',[1,0.25,0],'linewidth',.25); hold on;
 quiver3(X,Y,Z,zeros(size(Fx)),Fy,zeros(size(Fx)),'color',[1,0.25,0],'linewidth',.25); hold on;
 quiver3(X,Y,Z,zeros(size(Fz)),zeros(size(Fz)),Fz,'color',[1,0.25,0],'linewidth',.25);
-axis tight;
+
+% axis tight;
+% ylim([-0.5 1])
+% zlim([-0.5 1])
 
 % Plot Approx Friction Coefficient
 if exist('h2','var') && isvalid(h2), delete(h2);end
 h2 = figure('Color',[1 1 1]);
-subplot(3,1,1)
+subplot(4,1,1)
 stairs(phases(1:sample:end), 'LineWidth',2); 
 title('Phases','Interpreter', 'LaTex', 'FontSize',20)
 grid on;
-subplot(3,1,2)
+subplot(4,1,2)
 plot(approx_frict_x, 'LineWidth',2); hold on;
 plot(approx_frict_y, 'LineWidth',2); hold on;
 plot(approx_frict_z, 'LineWidth',2); 
 ylim([0 2])
-legend('x-normal','y-normal','z-normal')
+legend({'$f_n = f_x$','$f_n = f_y$','$f_n = f_z$'},'Interpreter','LaTex')
 plot(ones(length(approx_frict_x)), 'LineWidth',2, 'Color', [0 0 0]); hold on;
-title('Approximate Friction $\tilde{\mu} = (f_x^2+f_y^2)^{(-1/2)}/f_z$','Interpreter', 'LaTex', 'FontSize',20)
+title('Approximate Friction $\tilde{\mu} = (f_{t_1}^2+f_{t_1}^2)^{(-1/2)}/f_n$','Interpreter', 'LaTex', 'FontSize',20)
 grid on;
-subplot(3,1,3)
+subplot(4,1,3)
 plot(err_x, 'LineWidth',2); hold on;
 plot(err_y, 'LineWidth',2); hold on;
 plot(err_z, 'LineWidth',2); 
-legend({'$f_{x}/||f_{x}|| - v_{x}/||v_{x}||$','$f_{y}/||f_{y}|| - v_{y}/||v_{y}||$','$f_{z}/||f_{z}|| - v_{z}/||v_{z}||$'},'Interpreter', 'LaTex')
-title('Friction Plane Errors','Interpreter', 'LaTex', 'FontSize',20)
+legend({'$f_{x} \cdot v_{x}$','$f_{y} \cdot v_{y}$','$f_{z} \cdot v_{z}$'},'Interpreter', 'LaTex')
+title('Force-Velocity Dot Products','Interpreter', 'LaTex', 'FontSize',20)
+grid on;
+subplot(4,1,4)
+plot(forces', 'LineWidth',2);
+legend({'$f_x$','$f_y$','$f_z$'},'Interpreter','LaTex')
 grid on;
--- test-data/Rolling/load_rolling_demos.m
+++ test-data/Rolling/load_rolling_demos.m
@@ -28,11 +28,16 @@ for i=1:length(demos)
     true_states = True_states{i};
     for k = 1:size(Seq{demos(i)},1)  
         ids = segs(k,1):segs(k,2);
-        X_ = X(:,ids);
-        X_n =  X_;
-        X_n(9,:) =  -X_(8,:);
-        X_n(8,:) =  -X_(9,:);
-        Data_seq{j} = X_n';
+        X_ = X(:,ids);        
+        
+        % Rotate around Z to get correct force directions
+        for ii=1:length(X_)
+            f = X_(8:10,ii);
+            X_(8:10,ii) = rotz(-pi/2)*f;
+%             X_(8,ii) = 0;
+        end
+
+        Data_seq{j} = X_';
         True_states_seq{j} = true_states(1,ids)';
         j = j + 1;
     end
